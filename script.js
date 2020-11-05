52a2549195dd5f789a8bccbbf05979c80fec94b2
karintky <karintan221@gmail.com>
2020-11-05 19:06:18 +0800

    EDIT JS

---
+++ script.js
@@ -0,0 +1,15 @@
+// if 語法
+// if (布林值) { 程式區塊 }
+// 布林值為 true 才會執行 程式區塊(VERY IMPORTANT)
+
+
+
+if (true) {
+    console.log("我是判斷式");
+  }
+  
+  var price1 = 500;
+  
+  if (price1 >= 200) {
+    console.log("大於 200 塊的商品");
+  }
